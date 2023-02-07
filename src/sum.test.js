d949d7922940b0edcb4aa19e8f193250dd7b41f2
Osamu-yoshida123 <osamu.yoshida@fab.pasona.tech>
2023-02-07 12:12:46 +0900

    constの練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -10,3 +10,10 @@ test('プログラムコメントの練習', () => {
 expect(sum(1, 2)).toBe(3);
 
 });
+
+test('constの練習',() => {
+  const booktitle = "本";
+  const bookprice = 3000;
+  expect(booktitle).toBe("本");
+  expect(bookprice).toBe(3000);
+});
