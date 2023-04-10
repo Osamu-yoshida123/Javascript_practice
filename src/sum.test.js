9bf591a5fe25f408a04fb4acd1c9e91e62a42c47
Osamu-yoshida123 <osamu.yoshida@fab.pasona.tech>
2023-04-10 19:01:10 +0900

    ニューバージョン２

--- src/sum.test.js
+++ src/sum.test.js
@@ -10,7 +10,7 @@ console.log(bookTitle)
 bookCategory = "プログラミング";
 console.log(bookCategory)
 console.log(1);
-*/
+
 console.log(typeof "aaa");
 
 const objA = {};
@@ -24,8 +24,27 @@ console.log(1 === 1);
 let aa = 1;
 
 console.log(1 === aa);
+*/
 
+/*
+console.log(0 == false);
 
 console.log(1 == "1");
 
+console.log(10 == ["10"]);
+*/
+
+function double1(num) {return num * 2}
+function double2(n1,n2) {return n1+n2}
+function argumentsToArray(x, y) {return [x, y];}
+
+function echo(x = "デフォルト値") {return x;}
+
+/*
+console.log(double1(100))
+console.log(double2(1,2))
+console.log(argumentsToArray(1, 2))
+*/
+
+console.log(222)
 });
